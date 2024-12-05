ad1df75d4af7f5b73dac84b192e8cc76c5068538
Mohit Kumar <mohit105296@yahoo.com>
2024-12-04 20:11:00 -0800

    Add files via upload

---
+++ HashNode.cpp
@@ -0,0 +1,13 @@
+#include "HashNode.hpp"
+
+HashNode::HashNode(int value) : Node(value), cacheNode(nullptr) {
+    // Initialization of HashNode
+}
+
+HashNode::HashNode(int value, Node* newCacheNode) : Node(value), cacheNode(newCacheNode) {
+    // Initialization of HashNode
+}
+
+Node* HashNode::getCachePointer(){
+    return cacheNode;
+}
