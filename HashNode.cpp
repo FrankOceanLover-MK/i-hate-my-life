17b3ce5b4ad7a24127a28278e2ab22757546710e
Mohit Kumar <mohit105296@yahoo.com>
2024-12-05 16:55:04 -0800

    Update HashNode.cpp

--- HashNode.cpp
+++ HashNode.cpp
@@ -1,13 +1,48 @@
+/**
+* 
+* Mohit Kumar
+* Arpan Rakeshbhai
+* 12/03/2024
+* 
+* 
+* HashNode.cpp : Implements the HashNode class, which extends the Node class to include
+* a pointer to a corresponding cache node. This allows HashNodes to be linked
+* to nodes in a FIFO cache while maintaining key-value pairs in the hash table.
+* 
+* 12/03/2024 - Created by Mohit Kumar and Arpan Rakeshbhai
+* 12/04/2024 - Modified by Franco Barra
+* 
+*/
+
 #include "HashNode.hpp"
 
+/*  HashNode Constructor
+
+Initializes a HashNode with the given key value. This constructor sets up the
+node without linking it to any cache data by initializing the cacheNode pointer to nullptr.
+
+*/
 HashNode::HashNode(int value) : Node(value), cacheNode(nullptr) {
-    // Initialization of HashNode
+    // Initialization of HashNode without associated cache node
 }
 
+
+/*  HashNode Constructor (Overloaded)
+
+Initializes a HashNode with the given key value and a pointer to an associated
+cache node. This constructor links the HashNode to the corresponding Node in the cache.
+
+*/
 HashNode::HashNode(int value, Node* newCacheNode) : Node(value), cacheNode(newCacheNode) {
-    // Initialization of HashNode
+    // Initialization of HashNode with associated cache node
 }
 
-Node* HashNode::getCachePointer(){
+
+/*  getCachePointer
+
+Retrieves the pointer to the associated cache node stored in this HashNode.
+
+*/
+Node* HashNode::getCachePointer() {
     return cacheNode;
 }
